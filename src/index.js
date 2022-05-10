d34a34dc4a3c67d375ca50dfcd8cd12b563093ed
Stan <Stan9726@users.noreply.github.com>
2022-05-10 14:44:12 +0800

    Step VIII: Hooks

--- src/index.js
+++ src/index.js
@@ -171,9 +171,50 @@ function performUnitOfWork(fiber) {
   }
 }
 
+let wipFiber = null
+let hookIndex = null
+
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber
+  hookIndex = 0
+  wipFiber.hooks = []
+
   const children = [fiber.type(fiber.props)]
   reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+  const oldHook =
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks[hookIndex]
+
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: []
+  }
+
+  const actions = oldHook ? oldHook.queue : []
+  actions.forEach(action => {
+    hook.state = action(hook.state)
+  })
+
+  const setState = action => {
+    hook.queue.push(action)
+
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot
+    }
+
+    nextUnitOfWork = wipRoot
+    deletions = []
+  }
+
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  return [hook.state, setState]
 }
 
 function updateHostComponent(fiber) {
@@ -241,14 +282,24 @@ function reconcileChildren(wipFiber, elements) {
 
 const MyReact = {
   createElement,
-  render
+  render,
+  useState
 }
 
 /** @jsx MyReact.createElement */
-function App(props) {
-  return <h1>Hi {props.name}</h1>
+function Counter() {
+  const [count, setCount] = MyReact.useState(0)
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(c => c + 1)}>plus 1</button>
+      <button onClick={() => setCount(c => c - 1)}>minus 1</button>
+    </div>
+  )
 }
 
-const element = <App name="foo" />
+const element = <Counter />
+
 const container = document.getElementById('root')
 MyReact.render(element, container)
